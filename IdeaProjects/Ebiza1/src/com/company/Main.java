20a3b7cb746c9d5dc6a22c7243b8bde87a2e972e
Chidimma Uba <chidimmajuba@gmail.com>
2018-06-06 15:03:20 -0400

    I did Hashmap

---
+++ IdeaProjects/Ebiza1/src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random generate = new Random();
+        HashMap<String, String> map = new HashMap<String, String>();
+        String[] qualifier = {"Why do you say that ", "You seem to think that ", "So, you are concerned that "};
+        String[] hedges = {"Please tell me more ", "Many of my patients tell me the same thing ", "It is getting late, maybe we had better quit "};
+        String output = "";
+        boolean quit = false;
+        System.out.println("How are you feeling?");
+        String input = scan.nextLine();
+        do {
+            String[] a = input.split(" ");
+            map.put("I", "you");
+            map.put("me", "you");
+            map.put("my", "your");
+            map.put("am", "are");
+            for (int i = 0; i < a.length; i++) {
+                for (String key : map.keySet()) {
+                    if (a[i].equalsIgnoreCase(key)) {
+                        a[i] = map.get(key);
+                    }
+                }
+                output += a[i] + " ";
+            }
+            int qualiferIndex = generate.nextInt(qualifier.length);
+            int hedgeIndex = generate.nextInt(hedges.length);
+            String q = qualifier[qualiferIndex] + output + "?";
+            String h = hedges[hedgeIndex];
+            String[] m = {q, h};
+            System.out.println(m[generate.nextInt(m.length)]);
+            System.out.println("Enter your response here or Q to quit");
+            // use same name as the initial variable
+            input = scan.nextLine();
+            if (input.equalsIgnoreCase("q")) {
+                quit = true;
+            }
+            output = "";
+        } while (quit == false);
+
+    }
+}
+
